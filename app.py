25e52c0697bd4a8d04b882ddc8c97f5c52b3121b
Qiqi Jiang <itsqiqi@hotmail.com>
2016-07-06 01:51:26 +0000

    add sqlalchemy

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request, json, redirect, Markup, session
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask.ext.mysql import MySQL
 import os
 import re
+import sys
+sys.path.insert(0, '../db')
+from flask import Flask, render_template, request, json, redirect, Markup, session, Blueprint
+from werkzeug.security import generate_password_hash, check_password_hash
+from Models import db, User
 
 app = Flask(__name__, static_folder='public')
 app.secret_key = "vhainonWBU$J:TS*:$Yn4iaoew"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://iamqiqi:@localhost/blog2'
+db.init_app(app)
 
-mysql = MySQL()
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'iamqiqi'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'blog'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
-
-conn = mysql.connect()
-cursor = conn.cursor()
+@app.before_first_request
+def createDatabase():
+    db.create_all()
 
 @app.route("/")
 def main():
-    # if(cookie('loggedin')):
-    #     set some variable in the view
     return render_template('home/index.html')
 
 
@@ -43,29 +38,28 @@ def showUserPage():
 
 @app.route('/signIn', methods=['POST'])
 def signIn():
-    # create user code
     email = request.form['inputEmail']
     password = request.form['inputPassword']
     errors = []
-    if email and password:
-        query = 'select id, username, password from users where email = \'' + email + '\''
-        cursor.execute(query)
-        data = cursor.fetchone()
-        print data
-        if data == None:
-            errors.append("User does not exist")
-            return render_template("signin.html", errors=errors, email=email, password=password)
-        else:
-            if check_password_hash(data[2], password):
-                session['logged_in_userid'] = data[0]
-                session['logged_in_username'] = data[1]
-                return redirect('/')
-            else:
-                errors.append("Password is incorrect")
-                return render_template("session/signin.html", errors=errors, email=email, password=password)
-    else:
+    # pre check fields
+    if not (email and password):
         errors.append("One or more fields are missing")
         return render_template("session/signin.html", errors=errors, email=email, password=password)
+    elif validation_email == None:
+        errors.append("Invalid email address")
+        return render_template("signin.html", errors=errors, email=email, password=password)
+    else:
+        logged_in_user = User.query.filter_by(email=email).first()
+        if logged_in_user == None:
+            errors.append("User does not exist")
+            return render_template("signin.html", errors=errors, email=email, password=password)
+        elif not check_password_hash(logged_in_user.password, password):
+            errors.append("Password is incorrect")
+            return render_template("session/signin.html", errors=errors, email=email, password=password)
+        else:
+            session['logged_in_userid'] = logged_in_user.id
+            session['logged_in_username'] = logged_in_user.username
+            return redirect('/')
 
 
 @app.route('/signOut')
@@ -77,37 +71,43 @@ def signOut():
 @app.route('/signUp', methods=['POST'])
 def signUp():
     errors = []
-    name = request.form['inputName']
+    username = request.form['inputName']
     email = request.form['inputEmail']
     password = request.form['inputPassword']
     password2 = request.form['inputPassword2']
-    if name and email and password and password2:
+    # pre check fields
+    if not (username and email and password and password2):
+        errors.append("One or more fields are missing")
+        return render_template("session/signup.html", errors=errors, username=username, email=email, password=password, password2=password2)
+    else:
         validation_email = re.match('^[^@\s]+@[^@\s]+$', email)
         if validation_email == None:
             errors.append("Invalid email address")
-        if password and password2:
-            if password2 != password:
-                errors.append("Passwords don't match")
-    else:
-        errors.append("One or more fields are missing")
+        if password2 != password:
+            errors.append("Passwords don't match")
 
-    if len(errors) == 0:
-        hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
-        cursor.callproc('createUser', (name, email, hashed_password))
-        data = cursor.fetchall()
-        if len(data) is 0:
-            conn.commit()
-            return redirect('/')
+        if len(errors) != 0:
+            return render_template("session/signup.html", errors=errors, username=username, email=email, password=password, password2=password2)
         else:
-            errors.append(str(data[0]))
-            return render_template("session/signup.html", errors=errors, name=name, email=email, password=password, password2=password2)
-    else:
-        return render_template("session/signup.html", errors=errors, name=name, email=email, password=password, password2=password2)
+            hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
+            test_email = User.query.filter_by(email=email).first()
+            if test_email != None:
+                errors.append("this email is registed, user already exists")
+                return render_template("session/signup.html", errors=errors, username=username, email=email, password=password, password2=password2)
+            else:
+                test_username = User.query.filter_by(username=username).first()
+                if test_username != None:
+                    errors.append("username already exists, please pick another one")
+                    return render_template("session/signup.html", errors=errors, username=username, email=email, password=password, password2=password2)
+                else:
+                    user = User(username, email, password)
+                    db.session.add(user)
+                    db.session.commit()
+                    return redirect('/')
 
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 8080))
     app.run(host=host, port=port, debug=True)
-else:
-    print "app.py was imported, so I'm not running the server"
+
